8d8fb0339b8754933e13d6dd5d6dc86c0bac4d42
inkyant <80181074+inkyant@users.noreply.github.com>
2025-06-14 19:12:41 -0700

    use b1 urdf, publish joint states

--- src/publish.cpp
+++ src/publish.cpp
@@ -3,6 +3,8 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <sensor_msgs/JointState.h>
+#include <string.h>
 
 #include "unitree_legged_sdk/unitree_legged_sdk.h"
 
@@ -44,6 +46,32 @@ void Custom::RobotControl()
     udp.GetRecv(state);
 
     publishTF(state.position, state.imu.rpy);
+
+    // publish joints
+    static ros::NodeHandle node;
+    static ros::Publisher joint_pub = node.advertise<sensor_msgs::JointState>("joint_states", 1);
+
+
+    sensor_msgs::JointState joint_state;
+    joint_state.header.stamp = ros::Time::now();
+    joint_state.name.resize(12);
+    joint_state.position.resize(12);
+
+    const string leg_names[4] = {"FR", "FL", "RR", "RL"};
+
+    for (uint8_t i = 0; i < 4; i++) {
+      joint_state.name[i*3] = leg_names[i] + "_hip_joint";
+      joint_state.position[i*3] = state.motorState[i*3].q;
+
+      joint_state.name[i*3+1] = leg_names[i] + "_thigh_joint";
+      joint_state.position[i*3+1] = state.motorState[i*3+1].q;
+
+      joint_state.name[i*3+2] = leg_names[i] + "_calf_joint";
+      joint_state.position[i*3+2] = state.motorState[i*3+2].q;
+    }
+
+    //send the joint state
+    joint_pub.publish(joint_state);
 }
 
 void Custom::publishTF(std::array<float, 3> pos, std::array<float, 3> rpy)
