7aa97b730063991f811b4da689ffa151b43fcef7
Jake Lee <jlee211@ucsc.edu>
2025-11-19 15:36:16 -0800

    Updates to collect way more info; update EVAL_ID and SURFACE before running.

--- scripts/stream_joint_imu.cpp
+++ scripts/stream_joint_imu.cpp
@@ -10,6 +10,9 @@
 
 using namespace UNITREE_LEGGED_SDK;
 
+#define SURFACE 0
+#define EVAL_ID 0
+
 class Custom
 {
 public:
@@ -52,67 +55,33 @@ void Custom::RobotControl()
     udp.GetRecv(state);
 
     // Output all data as a CSV to stdout.
-    std::cout << timestamp << ",";
+    std::cout << timestamp << "," << SURFACE << ",";
+    std::cout << state.velocity[0] << ",";
+    std::cout << state.velocity[1] << ",";
+    std::cout << state.velocity[2] << ",";
+    std::cout << state.yawSpeed << ",";
+
+    std::cout << EVAL_ID << ",";
+
     for (uint8_t i = 0; i < 4; i++) {
-	std::cout << state.motorState[i].tauEst << ",";
-	std::cout << state.motorState[i+1].tauEst << ",";
-	std::cout << state.motorState[i+2].tauEst << ",";
+        std::cout << state.motorState[i].tauEst << ",";
+        std::cout << state.motorState[i+1].tauEst << ",";
+        std::cout << state.motorState[i+2].tauEst << ",";
     }
+
     std::cout << state.imu.accelerometer[0] << ",";
     std::cout 	    << state.imu.accelerometer[1] << ",";
     std::cout 	    << state.imu.accelerometer[2] << ",";
     std::cout 	    << state.imu.gyroscope[0] << ",";
     std::cout 	    << state.imu.gyroscope[1] << ",";
-    std::cout 	    << state.imu.gyroscope[2];
+    std::cout 	    << state.imu.gyroscope[2] << ",";
+    std::cout 	    << state.imu.quaternion[0] << ",";
+    std::cout 	    << state.imu.quaternion[1] << ",";
+    std::cout 	    << state.imu.quaternion[2] << ",";
+    std::cout 	    << state.imu.quaternion[3];
 
     std::cout << std::endl;
 
-    /*
-    motiontime++;
-    udp.GetRecv(state);
-    printf("%d  %f  %f\n", motiontime, state.motorState[FR_1].q, state.motorState[FR_1].dq);
-    // gravity compensation
-    cmd.motorCmd[FR_0].tau = -5.0f;
-    // cmd.motorCmd[FL_0].tau = +0.65f;
-    // cmd.motorCmd[RR_0].tau = -0.65f;
-    // cmd.motorCmd[RL_0].tau = +0.65f;
-
-    // float freq_Hz = 1;
-    // float freq_Hz = 2;
-    // float freq_Hz = 5;
-    // float freq_rad = freq_Hz * 2* M_PI;
-    // float t = dt*sin_count;
-
-    if( motiontime >= 500){
-        sin_count++;
-        float torque = (0 - state.motorState[FR_1].q)*10.0f + (0 - state.motorState[FR_1].dq)*1.0f;
-        // float torque = (0 - state.motorState[FR_1].q)*20.0f + (0 - state.motorState[FR_1].dq)*2.0f;
-        // float torque = (0 - state.motorState[FR_1].q)*40.0f + (0 - state.motorState[FR_1].dq)*2.0f;
-        // float torque = 2 * sin(t*freq_rad);
-        if(torque > 5.0f) torque = 5.0f;
-        if(torque < -5.0f) torque = -5.0f;
-        // if(torque > 15.0f) torque = 15.0f;
-        // if(torque < -15.0f) torque = -15.0f;
-
-        // cmd.motorCmd[FR_2].q = PosStopF;
-        // cmd.motorCmd[FR_2].dq = VelStopF;
-        // cmd.motorCmd[FR_2].Kp = 0;
-        // cmd.motorCmd[FR_2].Kd = 0;
-        // cmd.motorCmd[FR_2].tau = torque;
-
-        cmd.motorCmd[FR_1].q = PosStopF;
-        cmd.motorCmd[FR_1].dq = VelStopF;
-        cmd.motorCmd[FR_1].Kp = 0;
-        cmd.motorCmd[FR_1].Kd = 0;
-        cmd.motorCmd[FR_1].tau = torque;
-
-    }
-    // int res = safe.PowerProtect(cmd, state, 1);
-    // if(res < 0) exit(-1);
-
-    udp.SetSend(cmd);
-    */
-    //sleep(0.01);
 }
 
 int main(void)
@@ -122,9 +91,8 @@ int main(void)
     //         << "Press Enter to continue..." << std::endl;
     //std::cin.ignore();
     // Print headers.
-    std::cout << "Timestamp,FRHipT,FLHipT,RRHipT,RLHipT,FRThighT,FLThighT,RRThighT,RLThighT,FRCalfT,FLCalfT,RRCalfT,RLCalfT,IMUAccx,IMUAccy,IMUAccz,IMUGyrroll,IMUGyrpitch,IMUGyryaw\n";
+    std::cout << "time (ms),surface,forwardSpeed (m/s),sideSpeed (m/s),rotateSpeed (m?/s),yawSpeed(rad/s),eval_id,frHip t (Nm),FLHipT (Nm),RRHipT (Nm),RLHipT (Nm),FRThighT (Nm),FLThighT (Nm),RRThighT (Nm),RLThighT (Nm),FRCalfT (Nm),FLCalfT (Nm),RRCalfT (Nm),RLCalfT (Nm),IMUAccx,IMUAccy,IMUAccz,IMUGyrroll,IMUGyrpitch,IMUGyryaw,IMUQw,IMUQx,IMUQy,IMUQz\n";
 
-    //Custom custom(LOWLEVEL);
     Custom custom(HIGHLEVEL);
     InitEnvironment();
     LoopFunc loop_control("control_loop", custom.dt,    boost::bind(&Custom::RobotControl, &custom));
